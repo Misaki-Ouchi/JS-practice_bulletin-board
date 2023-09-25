817d29f58d23438ac8d18c25b3faaffc36578eba
ouchi <misaki.ouchi17@gmail.com>
2023-09-25 17:01:14 +0900

    form作成途中まで

--- src/ThreadComments.js
+++ src/ThreadComments.js
@@ -4,28 +4,54 @@ import "./main.css";
 import axios from "axios";
 // import useTitles from "./useTitles";
 
-const url = "http://localhost:3000/api/get/" + "comments";
+const url = "http://localhost:3000/api/get/comments";
 
-function ThreadComments() {
+function ThreadComments(props) {
+
   const [comments, setComments] = useState([]);
+  const [newComments, setNewComments] = useState({
+    title: "ちいかわを語る",
+    user_name: "",
+    message: "",
+    post_time: "",
+  });
 
   useEffect(() => {
     axios.get(url).then((response) => setComments(response.data));
   }, []);
 
+  const submitComment = (e) => {
+    setNewComments((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  // const submitComment = () => {
+
+  // axios.post(url, );
+  // }
+
   return (
-    <ul>
-      {Object.values(comments).map((val, index) => {
-        return val.board_id === 1 && (
-          <li key={index}>
-            <h3>
-              {val.id} 名前：{val.user_name}：{val.post_date}
-            </h3>
-            <p>{val.message}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul>
+        {Object.values(comments).map((val, index) => {
+          return (
+            val.title === "ちいかわを語る" && (
+              <li key={index}>
+                <h3>
+                  {val.comment_count} 名前：{val.user_name}：{val.post_date}
+                </h3>
+                <p>{val.message}</p>
+              </li>
+            )
+          );
+        })}
+      </ul>
+      名前：
+      <input type="text" name="user_name" />
+      email：
+      <input type="text" name="title" />
+      <br />
+      <textarea name="message" cols="80" rows="10"></textarea>
+      <button onClick="submitComment">書き込む</button>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./main.css";
-import TopTitle from './TopTitle';
-import ThreadTitles from './ThreadTitles';
-import ThreadComments from './ThreadComments';
+import TopTitle from "./TopTitle";
+import ThreadTitles from "./ThreadTitles";
+import ThreadComments from "./ThreadComments";
 // import App from './App';
 
 // const {titles} = ThreadTitles()
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <TopTitle />
     {/* <App /> */}
-    <button href="/">新規スレッドを書く</button><br/>
-    <ThreadTitles /><br/>
-    <ThreadComments />
+    <a href="./newTitle.html">新規スレッドを書く</a>
+    <br />
+    <ThreadTitles />
+    <br />
+    <ThreadComments title="ちいかわを語る" />
   </React.StrictMode>
 );
